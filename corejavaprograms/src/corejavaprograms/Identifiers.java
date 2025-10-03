e67b3ea2423a854b19fb817cc58545db8d1ef822
HP <HP@LAPTOP-PJ07AA5E>
2025-10-03 18:09:08 +0530

    Day1

---
+++ corejavaprograms/src/corejavaprograms/Identifiers.java
@@ -0,0 +1,15 @@
+package corejavaprograms;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="shanuu"; //use camelcase
+		System.out.println("value is : "+ studentName);
+
+	}
+
+}
