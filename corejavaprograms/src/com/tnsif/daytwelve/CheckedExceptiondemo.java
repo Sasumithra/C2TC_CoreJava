898ed3b2cd3e5ea30f5db6880b7c60b9fceb97a4
HP <HP@LAPTOP-PJ07AA5E>
2025-10-25 18:54:19 +0530

    day 12

---
+++ corejavaprograms/src/com/tnsif/daytwelve/CheckedExceptiondemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.daytwelve;
+import java.io.IOException;
+public class CheckedExceptiondemo {
+	public static void main(String[] args) {
+		try {
+			String str = CheckedException.acceptText();// reads line from console
+			System.out.println("Your string is " + str);
+		} catch (IOException e) {
+			System.err.println("An error occurred while reading input: " + e.getMessage());
+		}
+	}
+
+}
---
+++ corejavaprograms/src/com/tnsif/daytwelve/Invalidmarkexception.java
@@ -0,0 +1,12 @@
+package com.tnsif.daytwelve;
+
+public class Invalidmarkexception extends Exception{
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+}
