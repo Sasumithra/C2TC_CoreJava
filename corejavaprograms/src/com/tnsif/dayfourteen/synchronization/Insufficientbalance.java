1faef285af4c2eb0c046534b07ee9ce5f9e4435a
HP <HP@LAPTOP-PJ07AA5E>
2025-10-25 19:21:42 +0530

    day 14

---
+++ corejavaprograms/src/com/tnsif/dayfourteen/synchronization/Insufficientbalance.java
@@ -0,0 +1,12 @@
+package com.tnsif.dayfourteen.synchronization;
+
+public class Insufficientbalance extends Exception {
+
+	public Insufficientbalance() {
+		super("Insufficient balance in your account");
+	}
+
+	public Insufficientbalance(String message) {
+		super(message);
+	}
+}
